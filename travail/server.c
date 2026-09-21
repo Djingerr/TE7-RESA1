77d8623377d01bb755fd12b869bb81bdc2610d3f
Maxenzzz <max.paitard@gmail.com>
2026-09-21 17:16:10 +0200

    gestion de client unique avec server.c

--- travail/server.c
+++ travail/server.c
@@ -17,21 +17,24 @@ void die(int ret_value, const char *msg)
     }
 }
 
-int recv_all(int sock, void *buffer, size_t size)
+int read_on_socket(int sock, void* buffer, size_t length)
 {
-    size_t read_bytes = 0;
-    int ret_value;
-
-    while (read_bytes != size) {
-        ret_value = read(sock, (char *)buffer + read_bytes, size - read_bytes);
-        if (ret_value == 0) {
-            printf("Disconnected\n");
-            exit(EXIT_FAILURE);
+    int written_bytes = 0;
+    int to_write = length;
+    int ret_val;
+    while(written_bytes != to_write)
+    {
+        ret_val = read(sock, (char*)(buffer) + written_bytes, to_write - written_bytes);
+        if(ret_val == 0)
+        {
+            printf("disconnected\n");
+            printf("client disconnected");
+            return ret_val;
         }
-        die(ret_value, "reading");
-        read_bytes += ret_value;
+        die(ret_val, "reading msg header");
+        written_bytes += ret_val;
     }
-    return ret_value;
+    return ret_val;
 }
 
 int send_all(int sock, void *buffer, size_t size)
@@ -72,14 +75,14 @@ void echo_server(int sockfd) {
 	}
 }
 
-int handle_bind() {
+int handle_bind(char* server_port) {
 	struct addrinfo hints, *result, *rp;
 	int sfd;
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = AI_PASSIVE;
-	if (getaddrinfo(NULL, SERV_PORT, &hints, &result) != 0) {
+	if (getaddrinfo(NULL, server_port, &hints, &result) != 0) {
 		perror("getaddrinfo()");
 		exit(EXIT_FAILURE);
 	}
@@ -103,15 +106,20 @@ int handle_bind() {
 }
 
 int main(int argc, char* argv[]) {
+    
     if(argc != 2){
-        
+        printf("veuillez saisir le port du serveur\n");
+        exit(EXIT_FAILURE);
     }
-    int server_name = argv[1];
+    char* server_port = argv[1];
+
 	struct sockaddr cli;
-	int sfd, connfd;
+    
+    int sfd, connfd;
 	socklen_t len;
-	sfd = handle_bind();
-	if ((listen(sfd, SOMAXCONN)) != 0) {
+
+    sfd = handle_bind(server_port);
+    if ((listen(sfd, SOMAXCONN)) != 0) {
 		perror("listen()\n");
 		exit(EXIT_FAILURE);
 	}
@@ -120,7 +128,10 @@ int main(int argc, char* argv[]) {
 		perror("accept()\n");
 		exit(EXIT_FAILURE);
 	}
-	echo_server(connfd);
+    char buff[MSG_LEN] = {};
+
+	read_on_socket(connfd, (void*)&buff, MSG_LEN);
+    printf("message received : %s", buff);
 	close(sfd);
 	return EXIT_SUCCESS;
 }
