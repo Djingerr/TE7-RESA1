3928324b389c1c63af7e50838f974a9645440a8e
Maxenzzz <max.paitard@gmail.com>
2026-09-21 15:57:12 +0200

    modification de makefile
    intégration du lien vers le répertoire Include pour acceder à tous les fichier .h

---
+++ travail/server.c
@@ -0,0 +1,127 @@
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include "common.h"
+
+void die(int ret_value, const char *msg)
+{
+    if (ret_value < 0) {
+        perror(msg);
+        exit(EXIT_FAILURE);
+    }
+}
+
+int recv_all(int sock, void *buffer, size_t size)
+{
+    size_t read_bytes = 0;
+    int ret_value;
+
+    while (read_bytes != size) {
+        ret_value = read(sock, (char *)buffer + read_bytes, size - read_bytes);
+        if (ret_value == 0) {
+            printf("Disconnected\n");
+            exit(EXIT_FAILURE);
+        }
+        die(ret_value, "reading");
+        read_bytes += ret_value;
+    }
+    return ret_value;
+}
+
+int send_all(int sock, void *buffer, size_t size)
+{
+    size_t written_bytes = 0;
+    int ret_value;
+
+    while (written_bytes != size) {
+        ret_value = write(sock, (char *)buffer + written_bytes, size - written_bytes);
+
+        if (ret_value == 0) {
+            printf("Disconnected\n");
+            exit(EXIT_FAILURE);
+        }
+
+        die(ret_value, "writing");
+        written_bytes += ret_value;
+    }
+
+    return ret_value;
+}
+
+void echo_server(int sockfd) {
+	char buff[MSG_LEN];
+	while (1) {
+		// Cleaning memory
+		memset(buff, 0, MSG_LEN);
+		// Receiving message
+		if (recv(sockfd, buff, MSG_LEN, 0) <= 0) {
+			break;
+		}
+		printf("Received: %s", buff);
+		// Sending message (ECHO)
+		if (send(sockfd, buff, strlen(buff), 0) <= 0) {
+			break;
+		}
+		printf("Message sent!\n");
+	}
+}
+
+int handle_bind() {
+	struct addrinfo hints, *result, *rp;
+	int sfd;
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+	if (getaddrinfo(NULL, SERV_PORT, &hints, &result) != 0) {
+		perror("getaddrinfo()");
+		exit(EXIT_FAILURE);
+	}
+	for (rp = result; rp != NULL; rp = rp->ai_next) {
+		sfd = socket(rp->ai_family, rp->ai_socktype,
+		rp->ai_protocol);
+		if (sfd == -1) {
+			continue;
+		}
+		if (bind(sfd, rp->ai_addr, rp->ai_addrlen) == 0) {
+			break;
+		}
+		close(sfd);
+	}
+	if (rp == NULL) {
+		fprintf(stderr, "Could not bind\n");
+		exit(EXIT_FAILURE);
+	}
+	freeaddrinfo(result);
+	return sfd;
+}
+
+int main(int argc, char* argv[]) {
+    if(argc != 2){
+        
+    }
+    int server_name = argv[1];
+	struct sockaddr cli;
+	int sfd, connfd;
+	socklen_t len;
+	sfd = handle_bind();
+	if ((listen(sfd, SOMAXCONN)) != 0) {
+		perror("listen()\n");
+		exit(EXIT_FAILURE);
+	}
+	len = sizeof(cli);
+	if ((connfd = accept(sfd, (struct sockaddr*) &cli, &len)) < 0) {
+		perror("accept()\n");
+		exit(EXIT_FAILURE);
+	}
+	echo_server(connfd);
+	close(sfd);
+	return EXIT_SUCCESS;
+}
+
